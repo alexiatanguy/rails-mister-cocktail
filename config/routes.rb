9d100736c52a51c6088326d63b5ed7ff4bdd86df
Alexia Tanguy alexia.tanguy1@gmail.com <alexia.tanguy1@gmail.com>
2020-01-31 13:09:33 +0000

    corrected root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-root to: "cocktails/index"
+root to: "cocktails#index"
   resources :cocktails, only: [:new, :create, :index, :show, :edit, :update]
   resources :cocktails do
     resources :doses, only: [:new, :create, :edit, :update, :destroy]
